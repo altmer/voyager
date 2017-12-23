463e936ab4c61adef1045a01f60d9de240cb8a53
Andrey Marchenko <igendou@gmail.com>
2017-12-23 21:06:06 +0100

    Sessions functions

---
+++ lib/voyager/accounts/sessions.ex
@@ -0,0 +1,45 @@
+defmodule Voyager.Accounts.Sessions do
+  @moduledoc """
+    Functions to work with session
+  """
+  @auth_failed {:error, :auth_failed}
+
+  alias Voyager.Guardian
+  alias Voyager.Repo
+  alias Voyager.Accounts.User
+  alias Comeonin.Bcrypt
+
+  def authenticate(%{"email" => nil}),
+    do: @auth_failed
+  def authenticate(%{"email" => ""}),
+    do: @auth_failed
+  def authenticate(%{"email" => email, "password" => password}) do
+    with user <- Repo.get_by(User, email: String.downcase(email)),
+         true <- check_password(user, password) do
+      gen_token(user)
+    else
+      _ -> @auth_failed
+    end
+  end
+
+  def from_token(jwt) do
+    with {:ok, {claims, _}} <- Guardian.decode_and_verify(jwt),
+         {:ok, user} <- Guardian.resource_from_claims(claims) do
+      {:ok, user, claims["jti"]}
+    else
+      {:error, reason} -> {:error, reason}
+    end
+  end
+
+  def gen_token(nil),
+    do: @auth_failed
+  def gen_token(user) do
+    {:ok, jwt, _} = Guardian.encode_and_sign(user)
+    {:ok, user, jwt}
+  end
+
+  defp check_password(nil, _),
+    do: false
+  defp check_password(user, password),
+    do: Bcrypt.checkpw(password, user.encrypted_password)
+end
---
+++ test/voyager/accounts/session_test.exs
@@ -0,0 +1,94 @@
+defmodule Voyager.SessionsTest do
+  use Voyager.DataCase
+
+  import Voyager.Factory
+
+  alias Voyager.Guardian
+  alias Voyager.Repo
+  alias Voyager.Accounts.{AuthToken, Sessions}
+
+  describe "Sessions.authenticate/1" do
+    test "it returns failed auth when given email is nil" do
+      assert {:error, :auth_failed} = Sessions.authenticate(%{
+        "email" => nil,
+        "password" => "1232"
+      })
+    end
+
+    test "it returns failed auth when given email is empty" do
+      assert {:error, :auth_failed} = Sessions.authenticate(%{
+        "email" => "",
+        "password" => "1232"}
+      )
+    end
+
+    test "it returns failed auth when there is no user with given email" do
+      insert(:user)
+      assert {:error, :auth_failed} = Sessions.authenticate(
+        %{"email" => "some@email.com", "password" => "12345678"}
+      )
+    end
+
+    test "it returns failed auth when password is incorrect" do
+      user = insert(:user)
+      assert {:error, :auth_failed} = Sessions.authenticate(
+        %{"email" => user.email, "password" => "123456789"}
+      )
+    end
+
+    test "it returns ok with user and token when auth succeeded" do
+      user = insert(:user)
+      assert {:ok, auth_user, jwt} = Sessions.authenticate(
+        %{"email" => user.email, "password" => "12345678"}
+      )
+
+      assert auth_user.id == user.id
+
+      {
+        :ok,
+        {claims, _}
+      } = Guardian.decode_and_verify(jwt)
+
+      assert {:ok, token_user} = Guardian.resource_from_claims(claims)
+      assert token_user.id == user.id
+    end
+  end
+
+  describe "Sessions.from_token/1" do
+    test "it returns user if it still exists" do
+      user = insert(:user)
+      {:ok, jwt, _} = Guardian.encode_and_sign(
+        user, %{}, ttl: {1, :hours}
+      )
+
+      assert {:ok, token_user, _} = Sessions.from_token(jwt)
+      assert token_user.id == user.id
+    end
+
+    test "it returns error if token is not in DB" do
+      user = insert(:user)
+      {:ok, jwt, _} = Guardian.encode_and_sign(
+        user, %{}, ttl: {1, :hours}
+      )
+
+      Repo.delete_all(AuthToken)
+
+      assert {:error, :token_not_found} = Sessions.from_token(jwt)
+    end
+  end
+
+  describe "Sessions.gen_token/1" do
+    test "it returns error if user is nil" do
+      assert {:error, :auth_failed} = Sessions.gen_token(nil)
+    end
+
+    test "it returns user and correct jwt token for existing user" do
+      user = insert(:user)
+      assert {:ok, auth_user, jwt} = Sessions.gen_token(user)
+      assert {:ok, token_user, _} = Sessions.from_token(jwt)
+
+      assert auth_user.id == user.id
+      assert token_user.id == user.id
+    end
+  end
+end
